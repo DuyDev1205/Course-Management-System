a256b55ab309770d8badfe48ee149f863d492f83
zunbi10 <ofhuy710@gmail.com>
2023-11-30 08:19:58 +0700

    Thêm Enrollment,  Department, Assignment check và viết lại main

---
+++ Course Management System/Course Management System/Department.h
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <iostream>
+#include <list>
+#include "Course.h"
+
+class Department {
+private:
+    std::string departmentID;
+    std::string departmentName;
+    std::string departmentDescription;
+    std::list<Course*> coursesInDepartment;
+
+    // Tạo một danh sách lưu trữ các bộ môn
+    static std::list<Department*> departmentList;
+
+public:
+    Department(std::string deptID, std::string deptName, std::string deptDesc);
+
+    static void listAllDepartments(); // Hiển thị danh sách tất cả các bộ môn
+    static Department* searchDepartment(std::string deptID); // Tìm kiếm bộ môn
+    void viewDepartmentDetails(); // Hiển thị chi tiết bộ môn
+    static void addNewDepartment(std::string deptID, std::string deptName, std::string deptDesc); // Thêm bộ môn mới
+    static void deleteDepartment(std::string deptID); // Xóa bộ môn
+};
